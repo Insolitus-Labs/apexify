4c6b42bf7334e0e1c088a6056e3561fef93b526e
Avery Sinclair <averysinclair081@gmail.com>
2023-11-14 15:30:00 +0800

    feat(seo): add Open Graph and Twitter meta tags for better link previews

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,32 @@
 import "./globals.css"
 import ParticlesBackground from "../components/ParticlesBackground"
 import type { Metadata } from "next"
-import type React from "react" // Import React
+import type React from "react"
 
 export const metadata: Metadata = { 
   title: "APEXIFY",
   description: "AI-Powered Yield Farming Across Solana",
+  openGraph: {
+    title: "APEXIFY - AI-Powered Yield Farming",
+    description: "AI-driven yield optimization across Solana’s DeFi ecosystem.",
+    url: "https://apexify.com",
+    siteName: "APEXIFY",
+    images: [
+      {
+        url: "https://hebbkx1anhila5yf.public.blob.vercel-storage.com/apexify.jpg-B3vJz0v2kpg4SEucZOMbhmqjVzIx6G.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "APEXIFY Yield Farming",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "APEXIFY - AI-Powered Yield Farming",
+    description: "AI-driven yield optimization across Solana’s DeFi ecosystem.",
+    images: ["https://hebbkx1anhila5yf.public.blob.vercel-storage.com/apexify.jpg-B3vJz0v2kpg4SEucZOMbhmqjVzIx6G.jpeg"],
+  },
 }
 
 export default function RootLayout({
@@ -29,4 +50,3 @@ export default function RootLayout({
     </html>
   )
 }
-
