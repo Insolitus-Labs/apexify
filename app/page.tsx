91944ed7e762fb8a16f539c86efe6112882c3f9d
Avery Sinclair <averysinclair081@gmail.com>
2023-11-14 15:30:00 +0800

    feat(seo): add meta tags for better SEO optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,40 @@
 import { Suspense } from "react"
+import Head from "next/head"
 import dynamic from "next/dynamic"
 import Header from "../components/Header"
-import Footer from "../components/Footer" 
+import Footer from "../components/Footer"
 
 const DynamicHero = dynamic(() => import("../components/Hero"), { ssr: false })
-const DynamicDashboard = dynamic(() => import("../components/Dashboard"), { ssr: false }) 
+const DynamicDashboard = dynamic(() => import("../components/Dashboard"), { ssr: false })
 const DynamicWhyApexify = dynamic(() => import("../components/WhyApexify"), { ssr: false })
 const DynamicHowItWorks = dynamic(() => import("../components/HowItWorks"), { ssr: false })
 const DynamicSmartDeFiEngine = dynamic(() => import("../components/SmartDeFiEngine"), { ssr: false })
 const DynamicIntegrations = dynamic(() => import("../components/Integrations"), { ssr: false })
 const DynamicFAQs = dynamic(() => import("../components/FAQs"), { ssr: false })
 
-export default function Home() { 
+export default function Home() {
   return (
-    <main className="min-h-screen">
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <DynamicHero />
-      </Suspense>
-      <DynamicDashboard />
-      <Suspense fallback={<div>Loading...</div>}>
-        <DynamicWhyApexify />
-        <DynamicHowItWorks />
-        <DynamicSmartDeFiEngine />
-        <DynamicIntegrations />
-        <DynamicFAQs />
-      </Suspense>
-      <Footer />
-    </main>
+    <>
+      <Head>
+        <title>Apexify - Next-Gen Smart DeFi Solutions</title>
+        <meta name="description" content="Apexify is the ultimate Smart DeFi platform with advanced automation, security, and seamless integrations." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="min-h-screen">
+        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <DynamicHero />
+        </Suspense>
+        <DynamicDashboard />
+        <Suspense fallback={<div>Loading...</div>}>
+          <DynamicWhyApexify />
+          <DynamicHowItWorks />
+          <DynamicSmartDeFiEngine />
+          <DynamicIntegrations />
+          <DynamicFAQs />
+        </Suspense>
+        <Footer />
+      </main>
+    </>
   )
 }
-
